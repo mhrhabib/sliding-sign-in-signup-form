cd631d569d9ed72e4febec87b65d7dd91dd078c0
mhrhabib <mhrhabib39@gmail.com>
2020-09-08 23:23:18 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const signUpButton = document.getElementById('signUp');
+const signIpButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+
+signUpButton.addEventListener('click', () =>
+container.classList.add('right-panel-active')
+);
+
+
+signIpButton.addEventListener('click', () =>
+container.classList.remove('right-panel-active')
+);
+
+
